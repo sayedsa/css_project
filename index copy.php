f6da0c153bfd74b93614bda184b4f7bb7cef40ed
Sayed <sadatsayedibrahim@gmail.com>
2026-04-18 16:56:24 -0700

    3rd commit

---
+++ index copy.php
@@ -0,0 +1,109 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>3 Columns & Sidebar Layout</title>
+    <link rel="stylesheet" href="assets/css/main-style.css">
+
+
+</head>
+
+<body>
+    <?php include 'assets/header.php'; ?>
+
+    <section>
+
+
+    <div class="hero">
+        <img src="/assets/images/team.jpeg" alt="">
+    </div >
+        <div class="container">
+            <!-- Side Section -->
+
+
+            <!-- Main Section -->
+            <main class="main-section">
+                <h2 class="section"> Our Top Teams </h2>
+                <div class="main-content-row">
+                    <div class="column">
+                        <h3>Alpha</h3>
+                        <a href="#"> <img src="/assets/images/logo.png" alt="logo" class="logo"></a>
+                        <p>Lorem Ipsum is dummy text used to fill layouts in graphic design, publishing, and web development, allowing designers to focus on visual elements without distraction from readable content. Its purpose is to simulate natural text flow, making layouts appear realistic while the final content is not yet available.</p>
+                    </div>
+                    <div class="column">
+                        <h3>Bravo</h3>
+                        <a href="#"> <img src="/assets/images/logo.png" alt="logo" class="logo"></a>
+                        <p>Lorem Ipsum is dummy text used to fill layouts in graphic design, publishing, and web development, allowing designers to focus on visual elements without distraction from readable content. Its purpose is to simulate natural text flow, making layouts appear realistic while the final content is not yet available.</p>
+                    </div>
+                    <div class="column">
+                        <h3>Charlie</h3>
+                        <a href="#"> <img src="/assets/images/logo.png" alt="logo" class="logo"></a>
+                        <p>Lorem Ipsum is dummy text used to fill layouts in graphic design, publishing, and web development, allowing designers to focus on visual elements without distraction from readable content. Its purpose is to simulate natural text flow, making layouts appear realistic while the final content is not yet available.</p>
+                    </div>
+                </div>
+            </main>
+            <aside class="side-section">
+                <h2 class="side-sec-background">Side Section</h2>
+                <a href="#"> <img src="/assets/images/logo.png" alt="logo" class="logo"></a>
+                <p>Lorem Ipsum is dummy text used to fill layouts in graphic design, publishing, and web development, allowing designers to focus on visual elements without distraction from readable content. Its purpose is to simulate natural text flow, making layouts appear realistic while the final content is not yet available</p>
+            </aside>
+        </div>
+    </section>
+    <br>
+  
+<div class="column">
+  <h2 class="vidoe-title">Our Teams Vidoes</h2>
+</div>
+    <section class="main-content-row">
+      
+                <div class="main-content-row">
+                    
+                    <div class="column">
+                        <h3 class="h3">Column 1</h3>
+                        <iframe width="100%" height="100%"  class=" space" src="https://www.youtube.com/embed/ly36kn0ug4k?si=obVZQeBZjGdn3cWl" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+                        <p>Lorem Ipsum is dummy text used to fill layouts in graphic design, publishing, and web development, allowing designers to focus on visual elements without distraction from readable content. Its purpose is to simulate natural text flow, making layouts appear realistic while the final content is not yet available.</p>
+                    </div>
+                    <div class="column">
+                        <h3 class="h3">Column 2</h3>
+                        <iframe width="100%" height="100%"  class=" space" src="https://www.youtube.com/embed/ly36kn0ug4k?si=obVZQeBZjGdn3cWl" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+                        <p>Lorem Ipsum is dummy text used to fill layouts in graphic design, publishing, and web development, allowing designers to focus on visual elements without distraction from readable content. Its purpose is to simulate natural text flow, making layouts appear realistic while the final content is not yet available.</p>
+                    </div>
+
+                    <div class="column">
+                        <h3 class="h3">Column 3</h3>
+                        <iframe width="100%" height="100%"  class=" space" src="https://www.youtube.com/embed/ly36kn0ug4k?si=obVZQeBZjGdn3cWl" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+                        <p>Lorem Ipsum is dummy text used to fill layouts in graphic design, publishing, and web development, allowing designers to focus on visual elements without distraction from readable content. Its purpose is to simulate natural text flow, making layouts appear realistic while the final content is not yet available.</p>
+                    </div>
+                </div>
+    </section>
+ <br>
+ <br>
+ <br>
+ 
+    <section class="main-content-row">
+      
+                <div class="main-content-row">
+                    
+                    <div class="column">
+                        <h3 class="h3">Column 1</h3>
+                        <iframe width="100%" height="100%"  class=" space" src="https://www.youtube.com/embed/ly36kn0ug4k?si=obVZQeBZjGdn3cWl" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+                        <p>Lorem Ipsum is dummy text used to fill layouts in graphic design, publishing, and web development, allowing designers to focus on visual elements without distraction from readable content. Its purpose is to simulate natural text flow, making layouts appear realistic while the final content is not yet available.</p>
+                    </div>
+                    <div class="column">
+                        <h3 class="h3">Column 2</h3>
+                        <iframe width="100%" height="100%"  class=" space" src="https://www.youtube.com/embed/ly36kn0ug4k?si=obVZQeBZjGdn3cWl" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+                        <p>Lorem Ipsum is dummy text used to fill layouts in graphic design, publishing, and web development, allowing designers to focus on visual elements without distraction from readable content. Its purpose is to simulate natural text flow, making layouts appear realistic while the final content is not yet available.</p>
+                    </div>
+
+                    <div class="column">
+                        <h3 class="h3">Column 3</h3>
+                        <iframe width="100%" height="100%"  class=" space" src="https://www.youtube.com/embed/ly36kn0ug4k?si=obVZQeBZjGdn3cWl" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+                        <p>Lorem Ipsum is dummy text used to fill layouts in graphic design, publishing, and web development, allowing designers to focus on visual elements without distraction from readable content. Its purpose is to simulate natural text flow, making layouts appear realistic while the final content is not yet available.</p>
+                    </div>
+                </div>
+    </section>
+</body>
+
+</html>
